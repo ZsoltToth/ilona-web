0e23e1baba8e53f6272d73ba199d8c8ea14eaab6
DAZZLE <DAZZLE@10.115.1.119>
2016-07-16 12:56:53 +0200

    createDataBase.sql has been modified to identify dummy user properly on Windows

--- measurement/measurement-persist-mysql/src/main/resources/createDataBase.sql
+++ measurement/measurement-persist-mysql/src/main/resources/createDataBase.sql
@@ -3,3 +3,4 @@ create database Ilona;
 create user ilona identified by 'ilona';
 
 grant all on Ilona.* to 'ilona';
+grant all on Ilona.* to 'ilona'@'localhost' identified by 'ilona' with grant option;
