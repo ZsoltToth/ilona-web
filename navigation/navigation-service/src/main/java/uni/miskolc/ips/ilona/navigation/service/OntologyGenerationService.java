61dde3ae864a1651193e7c49dd47253fba3343df
dpkun <daniel.kun.92@gmail.com>
2016-08-01 14:24:16 +0200

    OntologyGenerationService has been defined OntologyInitializationController has been created

---
+++ navigation/navigation-service/src/main/java/uni/miskolc/ips/ilona/navigation/service/OntologyGenerationService.java
@@ -0,0 +1,9 @@
+package uni.miskolc.ips.ilona.navigation.service;
+
+import java.io.File;
+
+public interface OntologyGenerationService {
+
+	public File baseOntology();
+	public File generateRawOntology();
+}
